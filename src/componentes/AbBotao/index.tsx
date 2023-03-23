169e2a4f8bb3d362b02f95f843ac1dc9e3a66cd5
Thyago Vinicius Camilo Cunha <thyago.vccunha@gmail.com.br>
2023-03-23 12:28:26 -0300

    primeiro componente

---
+++ src/componentes/AbBotao/index.tsx
@@ -0,0 +1,23 @@
+import styled from "styled-components"
+
+const BotaoEstilizado = styled.button`
+  background: #eb9b00;
+  padding: 16px 32px;
+  border: 2px solid #eb9b00;
+  color: #fff;
+  font-size: 20px;
+  font-weight: 500;
+  cursor: pointer;
+  &:hover {
+    background: #b87900;
+    border: 2px solid #b87900;
+  }
+`
+
+export const AbBotao = () => {
+  return (
+    <BotaoEstilizado>
+      Clique aqui
+    </BotaoEstilizado>
+  )
+}
